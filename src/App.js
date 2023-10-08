5002f04e97461592d4ed2e4decf46f2a889d735b
mdsaifshaikh <shaikhmohammadsaif304@gmail.com>
2023-10-08 19:26:00 +0530

    add button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>welcome tp my react App</code> 
+           <code>welcome to my react App</code> 
         </p>
         <a
           className="App-link"
